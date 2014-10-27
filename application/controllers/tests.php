c9bf951f8f1112016900887bc006d7b934d04c63
Edward Scissorhands <edward_beats@hotmail.com>
2014-10-27 10:50:24 -0600

    new default controller2

---
+++ application/controllers/tests.php
@@ -0,0 +1,13 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Tests extends CI_Controller {
+
+	public function index()
+	{
+		echo "this is a test";
+	}
+
+}
+
+/* End of file tests.php */
+/* Location: ./application/controllers/tests.php */
