805e96e7d5b61b6381b8f25ea08e9a96fc064145
Edward Scissorhands <edward_beats@hotmail.com>
2014-10-28 12:33:06 -0600

    i forgot about the files

---
+++ application/views/gplus/login_client_enviroment.php
@@ -0,0 +1,4 @@
+<div class="row">
+	<p>This is an example of using the Login session button from Gplus</p>
+	<a href="#" title="gplus login" class="btn btn-primary">gplus login</a>
+</div>
---
+++ application/views/template/header.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Gplus CI <?php echo isset($title)? "| $title" : "" ?></title>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+	<!-- Optional theme -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css">
+	<!-- Latest compiled and minified JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<h3> <?php echo isset($title)? $title : "Gplus CI" ?> </h3>
+		</div>
